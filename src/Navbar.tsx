bd6664249d93a9bf96c88ed26efd14b359035924
Hellsrest <bishampandey100@gmail.com>
2025-01-19 11:47:35 +0545

    Title,Logo and Hamburger button change

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,56 +2,69 @@ import logo from "/logo.png";
 import {NavLink} from "react-router-dom";
 function Navbar() {
   return (
-    //this is change for final commit
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand d-flex align-items-center" href="/">
-              <img
-                src={logo}
-                width="50"
-                height="50"
-                className="d-inline-block align-top"
-                alt="no logo found"
-              />
-              <div className="ms-2"> ToDo List</div>
-            </a>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand d-flex align-items-center" href="/">
+    <img
+      src={logo}
+      width="50"
+      height="50"
+      className="d-inline-block align-top"
+      alt="no logo found"
+    />
+    <div className="ms-2"> ToDo List</div>
+  </a>
 
+  {/* Hamburger button */}
+  <button
+    className="navbar-toggler"
+    type="button"
+    data-bs-toggle="collapse" // Change to "data-bs-toggle" for Bootstrap 5
+    data-bs-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span className="navbar-toggler-icon"></span>
+  </button>
 
-
-        {/*this is a hamburger button only displayed when in mobile view or
-          responsive view.*/}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+  {/* Collapsed nav links */}
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ms-auto"> {/* Added ms-auto for alignment */}
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
         >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        {/*this is the end of the hamburger button.*/}
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="/aboutus"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          About Us
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="/contactus"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  </div>
+</nav>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-
-              <NavLink  to="/" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-                Home 
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/aboutus" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-                About Us
-              </NavLink>
-            </li>
-            <NavLink to="/contactus" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>          
-                Contact
-            </NavLink>
-          </ul>
-        </div>
-      </nav>
     </>
   );
 }
