14f5b9b7cd961f011c8a31bd8e12f662ab9f8ac0
Hellsrest <bishampandey100@gmail.com>
2025-01-19 10:59:52 +0545

    Initital Commit

---
+++ src/Navbar.tsx
@@ -0,0 +1,58 @@
+import logo from "/logo.png";
+import {NavLink} from "react-router-dom";
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand d-flex align-items-center" href="/">
+              <img
+                src={logo}
+                width="50"
+                height="50"
+                className="d-inline-block align-top"
+                alt="no logo found"
+              />
+              <div className="ms-2"> ToDo List</div>
+            </a>
+
+
+
+        {/*this is a hamburger button only displayed when in mobile view or
+          responsive view.*/}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        {/*this is the end of the hamburger button.*/}
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+
+              <NavLink  to="/" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                Home 
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/aboutus" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                About Us
+              </NavLink>
+            </li>
+            <NavLink to="/contactus" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>          
+                Contact
+            </NavLink>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
